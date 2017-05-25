42a4e43c7b7c786faa5813e41b48d10644f85c4f
tarasov-dmitriy <tarasov.dmitriy@gmail.com>
2017-05-25 19:20:38 +0400

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -6,12 +6,15 @@
  *
  */
 
-package com.example.lilia.jumamubarak;
+package com.example.admin.jumamubarak;
+
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+
+import java.text.NumberFormat;
 
 /**
  * This app displays an order form to order coffee.
@@ -28,7 +31,25 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(1);
+        int quaintity = 2;
+        display(quaintity);
+        displayPrice(quaintity * 5);
+    }
+
+    /** incrementing
+     * when plus button is clicked
+     */
+    public void increment(View view) {
+        int quantity = 3;
+        display(quantity);
+    }
+
+    /** decrementing
+     * when minus button is clicked
+     */
+    public void decrement(View view){
+        int quantity = 1;
+        display(quantity);
     }
 
     /**
@@ -38,4 +59,14 @@ public class MainActivity extends AppCompatActivity {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
+
+
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
 }
