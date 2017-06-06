7518b3cd42a7a405b1d0b2b93f68a8dcd9a64cb6
tarasov-dmitriy <tarasov.dmitriy@gmail.com>
2017-06-06 18:20:22 +0400

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,27 +1,18 @@
-/**
- * IMPORTANT: Add your package below. Package name can be found in the project's AndroidManifest.xml file.
- * This is the package name our example uses:
- *
- * package com.example.android.justjava; 
- *
- */
-
-package com.example.admin.jumamubarak;
+package com.example.android.justjava;
 
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
-
-import java.text.NumberFormat;
 
 /**
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
 
-    int quantity = 1;
+    int quantity = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,51 +24,73 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int price = quantity * 5;
-        String priceMessage = "Total: $" + price;
-        priceMessage = priceMessage + "\nThank You!"
+        CheckBox whippedCream = (CheckBox) findViewById(R.id.whipped_cream);
+        boolean hasWhippedCream = whippedCream.isChecked();
+        int price = calculatePrice(quantity, 5);
+        //String priceMessage = "Total: $" + price;
+        //priceMessage = priceMessage + "\nThank You!";
+        String priceMessage = createOrderSummary(price, hasWhippedCream);
         displayMessage(priceMessage);
+    }
+
+    /**
+     * This method use to display receipt
+     *
+     * @param price is price of 1 cup of coffee
+     */
+
+    public String createOrderSummary(int price, boolean hasWhippedCream) {
+        String priceMessage = "Name: Kaptain Kunal";
+        priceMessage += "\nAdd whipped cream? " + hasWhippedCream;
+        priceMessage += "\nQuantity: " + quantity;
+        priceMessage += "\nTotal: $" + price;
+        priceMessage += "\nThank you!";
+        return priceMessage;
+    }
+
+    /**
+     * Calculates the price of the order.
+     *
+     * @param quantity    is the number of cups of coffee ordered
+     * @param pricePerCup is the price per 1 cup of coffee
+     * @return price is the quantity * pricePerCup
+     */
+    private int calculatePrice(int quantity, int pricePerCup) {
+        return quantity * pricePerCup;
     }
 
     /**
      * This method displays the given text on the screen.
      */
     private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(message);
+        TextView OrderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        OrderSummaryTextView.setText(message);
     }
 
-    /** incrementing
+    /**
+     * incrementing
      * when plus button is clicked
      */
     public void increment(View view) {
         quantity = quantity + 1;
-        display(quantity);
+        displayQuantity(quantity);
     }
 
-    /** decrementing
+    /**
+     * decrementing
      * when minus button is clicked
      */
-    public void decrement(View view){
-        quantity = quantity + 1;
-        display(quantity);
+    public void decrement(View view) {
+        quantity = quantity - 1;
+        displayQuantity(quantity);
     }
 
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
+    private void displayQuantity(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
-    }
-
-
-    /**
-     * This method displays the given price on the screen.
-     */
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 
 }
